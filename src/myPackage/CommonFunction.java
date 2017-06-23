a1b960ab9fcabf90630fad137a36e06e97bd110e
thanh huyền <smartmouse.fun@gmail.com>
2017-06-23 13:20:35 +0700

    03 [Common Funtions] drop down box

--- src/myPackage/CommonFunction.java
+++ src/myPackage/CommonFunction.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * Created by thanh huyền on 23-Jun-17.
@@ -29,21 +30,31 @@ public class CommonFunction {
 //        Thread.sleep(5000);
 
         //using webElement
-        WebElement element = driver.findElement(By.linkText("Newer Post"));
-        element.click();
-        Thread.sleep(5000);
-        driver.navigate().back();
-        Thread.sleep(5000);
-        Boolean isPresent = driver.findElements(By.className("home-link")).size() > 0;
-        if(isPresent) {
-            System.out.println("pass");
-        }
-        else {
-            System.out.println("fail");
-        }
-        Thread.sleep(5000);
+//        WebElement element = driver.findElement(By.linkText("Newer Post"));
+//        element.click();
+//        Thread.sleep(5000);
+//        driver.navigate().back();
+//        Thread.sleep(5000);
+//        Boolean isPresent = driver.findElements(By.className("home-link")).size() > 0;
+//        if(isPresent) {
+//            System.out.println("pass");
+//        }
+//        else {
+//            System.out.println("fail");
+//        }
+//        Thread.sleep(5000);
+
+        //drop down box
+        // maximize the browser
+        driver.manage().window().maximize();
+
+        Select orderBy = new Select(driver.findElement(By.xpath("//div/select")));
+        orderBy.selectByVisibleText("Saab");
+        orderBy.selectByIndex(1);
+        orderBy.selectByValue("Russia");
+
 
 //        close browser
-        driver.close();
+//        driver.close();
     }
 }
